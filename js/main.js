4abc7d5a12a436af0dd441ca923572f0370532ee
mdabdulripon <mdabdulripon@gmail.com>
2017-04-08 00:11:02 -0400

    fix all mojor bug.

--- js/main.js
+++ js/main.js
@@ -93,15 +93,6 @@ jQuery(function($) {
 		$('header').height($(window).height());
 	});
 
-	// Pricing Box Click Event
-	$('.pricing .box-main').click(function() {
-		$('.pricing .box-main').removeClass('active');
-		$('.pricing .box-second').removeClass('active');
-		$(this).addClass('active');
-		$(this).next($('.box-second')).addClass('active');
-		$('#pricing').css("background-image", "url(" + $(this).data('img') + ")");
-		$('#pricing').css("background-size", "cover");
-	});
 
 	// Mobile Nav
 	$('body').on('click', 'nav .navbar-toggle', function() {
@@ -144,33 +135,7 @@ jQuery(function($) {
 			}
 		}
 	});
-
-	function centerModal() {
-		$(this).css('display', 'block');
-		var $dialog = $(this).find(".modal-dialog"),
-			offset = ($(window).height() - $dialog.height()) / 2,
-			bottomMargin = parseInt($dialog.css('marginBottom'), 10);
-
-		// Make sure you don't hide the top part of the modal w/ a negative margin
-		// if it's longer than the screen height, and keep the margin equal to 
-		// the bottom margin of the modal
-		if (offset < bottomMargin) offset = bottomMargin;
-		$dialog.css("margin-top", offset);
-	}
-
-	$('.modal').on('show.bs.modal', centerModal);
-
-	$('.modal-popup .close-link').click(function(event){
-		event.preventDefault();
-		$('#modal1').modal('hide');
-	});
-
-	$(window).on("resize", function() {
-		$('.modal:visible').each(centerModal);
-	});
 });
-
-
 
 
 // HOVER EFFECT
